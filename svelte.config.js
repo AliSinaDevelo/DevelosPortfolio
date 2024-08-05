c65d61a5a4bd50c88f3b07bbaeeee000f8841249
AliSina Karimi <100498538+AliSinaDevelo@users.noreply.github.com>
2024-08-05 14:53:27 +0200

    final before first deploy

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,8 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
+    // appDir in build/_app
+    appDir: 'app',
     adapter: adapter({
       pages: 'build',
       assets: 'build',
