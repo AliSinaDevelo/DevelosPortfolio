9e7913f4e215282b2898acc644f6527a17d1e762
AliSina Karimi <100498538+AliSinaDevelo@users.noreply.github.com>
2024-08-05 14:49:11 +0200

    final before first deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
-import adapter from '@sveltejs/adapter-static'; // or another appropriate adapter
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter({
-      pages: 'build',  // Ensure this matches the publish_dir in your GitHub Actions config
+      pages: 'build',
       assets: 'build',
-      fallback: null
+      fallback: '200.html', // Specify the fallback file
+      precompress: false,
+      strict: true
     }),
     paths: {
       base: process.env.NODE_ENV === 'development' ? '' : process.env.BASE_PATH,
